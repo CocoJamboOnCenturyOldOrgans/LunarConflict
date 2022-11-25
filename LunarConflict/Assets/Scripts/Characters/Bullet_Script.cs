0d49b36d83f342378219bcf12a388d4e73b3e8df
Daniel <dagohar246@gmail.com>
2022-11-25 10:14:26 +0100

    fix animators to fit existing scripts

--- LunarConflict/Assets/Scripts/Characters/Bullet_Script.cs
+++ LunarConflict/Assets/Scripts/Characters/Bullet_Script.cs
@@ -1,6 +1,4 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Bullet_Script : MonoBehaviour
@@ -8,15 +6,14 @@ public class Bullet_Script : MonoBehaviour
     [SerializeField] private int speed;
     [SerializeField] private int damage;
 
-    void Start() => StartCoroutine("Disappear");
-    void Update() => transform.Translate(Vector3.up * (speed * Time.deltaTime));
+    void Start() => StartCoroutine(Disappear());
+    void FixedUpdate() => transform.Translate(Vector3.up * (speed * Time.deltaTime));
 
     private void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.tag == "PlayerUnit" || col.tag == "EnemyUnit")
-        {
-            col.gameObject.GetComponent<GenericUnit_Script>().GotHit(damage);
-        }
+        if (col.gameObject.TryGetComponent<GenericUnit_Script>(out var unitScript))
+            unitScript.GotHit(damage);
+        
         Destroy(this.gameObject);
     }
     
