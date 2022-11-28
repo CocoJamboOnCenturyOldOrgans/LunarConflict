aa7d06b1e8dc165aa23667cd6f4c756f0d7691b2
Daniel <dagohar246@gmail.com>
2022-11-28 19:20:26 +0100

    Make fixes to animations and prefabs
    - deleted unnecessary files like unused animators, animation clips, prefabs
    - changed animation names for readability reasons
    - added death animations to lunar rovers
    - completed lunar rovers animation controllers
    - changed ground box collider
    - add temporary teleports animations in RoverUnitScript.cs on lunar rovers death state

--- LunarConflict/Assets/Scripts/Characters/RoverUnitScript.cs
+++ LunarConflict/Assets/Scripts/Characters/RoverUnitScript.cs
@@ -4,5 +4,27 @@ using UnityEngine;
 
 public class RoverUnitScript : GenericUnitScript
 {
+	[SerializeField] private GameObject teleport;
 
+	public void OpenTeleport()
+	{
+		StartCoroutine(CreateTeleport());
+	}
+
+	private IEnumerator CreateTeleport()
+	{
+		yield return null;
+        
+		var startScale = teleport.transform.localScale;
+		teleport.transform.localScale /= 100.0f;
+		var increaseVal = teleport.transform.localScale;
+        
+		teleport.SetActive(true);
+
+		while (teleport.transform.localScale.magnitude < startScale.magnitude)
+		{
+			teleport.transform.localScale += increaseVal;
+			yield return new WaitForSeconds(0.005f);
+		}
+	}
 }
