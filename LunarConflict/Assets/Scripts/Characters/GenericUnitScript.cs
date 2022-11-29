850ad45efcd6416753ed0f63ed1dc1857c61917e
Skeletonek <skeleton0199@gmail.com>
2022-11-29 22:05:58 +0100

    Fix units walking in and shooting teammates
    #22 - Units now stop when teammate is blocking a path
    #21 - Bullets ignore unit teammates

--- LunarConflict/Assets/Scripts/Characters/GenericUnitScript.cs
+++ LunarConflict/Assets/Scripts/Characters/GenericUnitScript.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GenericUnitScript : MonoBehaviour
@@ -13,29 +14,40 @@ public class GenericUnitScript : MonoBehaviour
     [SerializeField] protected int unitCost;
     [SerializeField] protected Transform bulletParent;
     [SerializeField] protected GameObject bulletPrefab;
+    
     private Animator _animator;
+    private LayerMask _mask;
+    private Vector2 _movementDirection;
+    private float _localScaleX;
 
     protected void Start() 
     {   
         _animator = GetComponent<Animator>();
+        _mask = LayerMask.GetMask("Unit");
         _animator.SetBool("play", true);
-        
+
+        _movementDirection = russian ? Vector2.left : Vector2.right;
+        _localScaleX = transform.localScale.x;
         speed = russian ? speed * -1 : speed;
-        attackRange = russian ? attackRange * -1 : attackRange;
     }
 
     protected void Update()
     {
-        if (_animator.GetCurrentAnimatorStateInfo(0).IsName("walking") || _animator.GetCurrentAnimatorStateInfo(0).IsName("driving"))
+        if (_animator.GetCurrentAnimatorStateInfo(0).IsName("walking") ||
+            _animator.GetCurrentAnimatorStateInfo(0).IsName("driving"))
             transform.Translate(Vector3.right * (speed * Time.deltaTime));
         _animator.SetBool("attackMode", CanAttack());
-        
+        _animator.SetBool("play", CanWalk());
         _animator.SetBool("dying", health <= 0 ? true : false);
     }
     
     private bool CanAttack()
     {
-        RaycastHit2D[] hit = Physics2D.RaycastAll(transform.position, Vector2.right, attackRange);
+        RaycastHit2D[] hit = Physics2D.RaycastAll(
+            transform.position, 
+            _movementDirection,
+            attackRange, 
+            _mask);
         foreach (var x in hit)
         {
             if (x.collider.CompareTag(russian ? "PlayerUnit" : "EnemyUnit") && x.collider.gameObject != gameObject)
@@ -49,7 +61,13 @@ public class GenericUnitScript : MonoBehaviour
     
     private bool CanWalk()
     {
-        throw new NotImplementedException();
+        LayerMask mask = LayerMask.GetMask("Unit");
+        RaycastHit2D hit = Physics2D.Raycast(
+            transform.position, 
+            _movementDirection, 
+            _localScaleX, 
+            _mask);
+        return hit.collider.IsUnityNull();
     }
     
     public void Shoot()
@@ -64,5 +82,6 @@ public class GenericUnitScript : MonoBehaviour
     {
         Destroy(gameObject);
     }
+    
     public void GotHit(int damage) => health -= damage;
 }
