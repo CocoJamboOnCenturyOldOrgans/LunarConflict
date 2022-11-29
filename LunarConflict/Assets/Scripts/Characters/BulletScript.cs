71254227a2abf828e42c5228e442e281aab5c448
Skeletonek <skeleton0199@gmail.com>
2022-11-29 22:05:58 +0100

    Fix units walking in and shooting teammates
    #22 - Units now stop when teammate is blocking a path
    #21 - Bullets ignore unit teammates

--- LunarConflict/Assets/Scripts/Characters/BulletScript.cs
+++ LunarConflict/Assets/Scripts/Characters/BulletScript.cs
@@ -8,16 +8,28 @@ public class BulletScript : MonoBehaviour
     [SerializeField] private int speed;
     [SerializeField] private int damage;
 
-    void Start() => StartCoroutine(Disappear());
+    private string _ignoreTag;
+
+    void Start()
+    {
+        _ignoreTag = (transform.rotation.z < 0 ? "PlayerUnit" : "EnemyUnit");
+        Debug.Log(_ignoreTag);
+        StartCoroutine(Disappear());
+    }
+
     void Update() => transform.Translate(Vector3.up * (speed * Time.deltaTime));
 
     private void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.TryGetComponent(out GenericUnitScript unitScript))
+        if (!col.gameObject.CompareTag(_ignoreTag))
         {
-            unitScript.GotHit(damage);
+            if (col.TryGetComponent(out GenericUnitScript unitScript))
+            {
+                unitScript.GotHit(damage);
+            }
+
+            Destroy(this.gameObject);
         }
-        Destroy(this.gameObject);
     }
     
     private IEnumerator Disappear()
