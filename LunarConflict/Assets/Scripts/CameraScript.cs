662d1fdf60c21532e21bee9e1e1ce7e907ebfb47
Skeletonek <skeleton0199@gmail.com>
2022-12-04 20:17:44 +0100

    Improve camera movement

--- LunarConflict/Assets/Scripts/CameraScript.cs
+++ LunarConflict/Assets/Scripts/CameraScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,10 +6,17 @@ using UnityEngine.InputSystem;
 
 public class CameraScript : MonoBehaviour
 {
+    [SerializeField] private float cameraSpeed;
+    private Vector3 _cameraLookVector3;
+    public void Update()
+    {
+        transform.position += _cameraLookVector3 * (cameraSpeed * Time.deltaTime);
+    }
+
     public void MoveCamera(InputAction.CallbackContext context)
     {
         var vector = context.ReadValue<Vector2>();
-        transform.position += new Vector3(vector.x, vector.y, 0);
-        Debug.Log(vector);
+        _cameraLookVector3 = new Vector3(vector.x, vector.y, 0);
     }
+    
 }
