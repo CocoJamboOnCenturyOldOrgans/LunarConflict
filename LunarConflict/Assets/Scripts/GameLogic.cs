21fb110b1c1a2465a98c0e39e3560dfd32cb230c
Skeletonek <skeleton0199@gmail.com>
2022-12-07 09:49:10 +0100

    Further UI code refactoring

--- LunarConflict/Assets/Scripts/GameLogic.cs
+++ LunarConflict/Assets/Scripts/GameLogic.cs
@@ -2,10 +2,41 @@ using UnityEngine;
 
 public class GameLogic : MonoBehaviour
 {
-    public GameObject game;
-    
+    private GameUIScript _uiScript;
+
+    // Start is called before the first frame update
     void Start()
     {
-        game = transform.gameObject;
+        _uiScript = FindObjectOfType<GameUIScript>();
+
+        if (SettingsScript.SideIsSoviet)
+            ChangeToSoviet();
+        else
+            ChangeToUsa();
+        
+        #warning This is temporary to view UI changes on different sides.
+        _uiScript.changed = true;
+    }
+
+    public void ChangeToUsa()
+    {
+        SettingsScript.UIMoneyMark = "$";
+        SettingsScript.AstronautShopPositive = Resources.Load<Sprite>("SpritesShop/usa_astronaut_Icon");
+        SettingsScript.AstronautShopNegative = Resources.Load<Sprite>("SpritesShop/usa_astronaut_Icon_LowMoney");
+        SettingsScript.RoverShopPositive = Resources.Load<Sprite>("SpritesShop/lunar_rover_usa_Icon");
+        SettingsScript.RoverShopNegative = Resources.Load<Sprite>("SpritesShop/lunar_rover_usa_Icon_LowMoney");
+        SettingsScript.AstronautName = "Astronaut";
+        SettingsScript.RoverName = "Lunar Rover";
+    }
+
+    public void ChangeToSoviet()
+    {
+        SettingsScript.UIMoneyMark = "₽";
+        SettingsScript.AstronautShopPositive = Resources.Load<Sprite>("SpritesShop/soviet_astronaut_Icon");
+        SettingsScript.AstronautShopNegative = Resources.Load<Sprite>("SpritesShop/soviet_astronaut_Icon_LowMoney");
+        SettingsScript.RoverShopPositive = Resources.Load<Sprite>("SpritesShop/lunar_rover_soviet_Icon");
+        SettingsScript.RoverShopNegative = Resources.Load<Sprite>("SpritesShop/lunar_rover_soviet_Icon_LowMoney");
+        SettingsScript.AstronautName = "Astronaut";
+        SettingsScript.RoverName = "Lunar Rover";
     }
 }
