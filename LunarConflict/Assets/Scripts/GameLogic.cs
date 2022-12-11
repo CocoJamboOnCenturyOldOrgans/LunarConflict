c6055c4a652b3b9b16b826f4a5197ab1a621f889
Skeletonek <skeleton0199@gmail.com>
2022-12-11 17:20:39 +0100

    Unit buttons for shop and statistics refactor

--- LunarConflict/Assets/Scripts/GameLogic.cs
+++ LunarConflict/Assets/Scripts/GameLogic.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class GameLogic : MonoBehaviour
 {
+    public GameObject game;
+    public bool playingAsRussian = false;
+    
     private GameUIScript _uiScript;
 
     // Start is called before the first frame update
