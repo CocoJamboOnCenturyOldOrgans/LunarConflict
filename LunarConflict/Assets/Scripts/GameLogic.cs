41a39e8d9a01fbb61e8091988357abf015108c5c
Skeletonek <skeleton0199@gmail.com>
2022-12-14 09:17:45 +0100

    Refactor GameUIScript

--- LunarConflict/Assets/Scripts/GameLogic.cs
+++ LunarConflict/Assets/Scripts/GameLogic.cs
@@ -3,8 +3,6 @@ using UnityEngine;
 public class GameLogic : MonoBehaviour
 {
     public GameObject game;
-    public bool playingAsRussian = false;
-    
     private GameUIScript _uiScript;
 
     // Start is called before the first frame update
@@ -16,30 +14,15 @@ public class GameLogic : MonoBehaviour
             ChangeToSoviet();
         else
             ChangeToUsa();
-        
-        #warning This is temporary to view UI changes on different sides.
-        _uiScript.changed = true;
     }
 
     public void ChangeToUsa()
     {
-        SettingsScript.UIMoneyMark = "$";
-        SettingsScript.AstronautShopPositive = Resources.Load<Sprite>("SpritesShop/usa_astronaut_Icon");
-        SettingsScript.AstronautShopNegative = Resources.Load<Sprite>("SpritesShop/usa_astronaut_Icon_LowMoney");
-        SettingsScript.RoverShopPositive = Resources.Load<Sprite>("SpritesShop/lunar_rover_usa_Icon");
-        SettingsScript.RoverShopNegative = Resources.Load<Sprite>("SpritesShop/lunar_rover_usa_Icon_LowMoney");
-        SettingsScript.AstronautName = "Astronaut";
-        SettingsScript.RoverName = "Lunar Rover";
+        SettingsScript.UIMoneyMark = "$"; 
     }
 
     public void ChangeToSoviet()
     {
         SettingsScript.UIMoneyMark = "₽";
-        SettingsScript.AstronautShopPositive = Resources.Load<Sprite>("SpritesShop/soviet_astronaut_Icon");
-        SettingsScript.AstronautShopNegative = Resources.Load<Sprite>("SpritesShop/soviet_astronaut_Icon_LowMoney");
-        SettingsScript.RoverShopPositive = Resources.Load<Sprite>("SpritesShop/lunar_rover_soviet_Icon");
-        SettingsScript.RoverShopNegative = Resources.Load<Sprite>("SpritesShop/lunar_rover_soviet_Icon_LowMoney");
-        SettingsScript.AstronautName = "Astronaut";
-        SettingsScript.RoverName = "Lunar Rover";
     }
 }
