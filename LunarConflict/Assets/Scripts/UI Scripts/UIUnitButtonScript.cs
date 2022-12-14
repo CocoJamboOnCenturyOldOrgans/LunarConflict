c60399f92403bf867dfae3a447d9e5e2ee98e700
Daniel Leśniak <43370195+Dagohar@users.noreply.github.com>
2022-12-14 20:04:15 +0100

    Revert "Refactor GameUIScript"
    This reverts commit 70ae7e3ae630082d9a1481d71da58e89767e6c6b.

--- LunarConflict/Assets/Scripts/UI Scripts/UIUnitButtonScript.cs
+++ LunarConflict/Assets/Scripts/UI Scripts/UIUnitButtonScript.cs
@@ -1,80 +1,59 @@
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class UIUnitButtonScript : MonoBehaviour
 {
     private GameUIScript _uiScript;
     private GameLogic _gameLogic;
-    private PlayerScript _playerScript;
-    
-    private GenericUnitScript _currentUnit;
-    private Sprite _currentUnitIconBuyable;
-    private Sprite _currentUnitIconNonBuyable;
-    
-    private Image _buttonImage;
-    
-    [SerializeField] private Text unitPriceText;
-    
     [SerializeField] private GenericUnitScript unitUsa;
     [SerializeField] private GenericUnitScript unitRussian;
-    [SerializeField] private Sprite UnitIconBuyableUsa;
-    [SerializeField] private Sprite UnitIconNonBuyableUsa;
-    [SerializeField] private Sprite UnitIconBuyableRussian;
-    [SerializeField] private Sprite UnitIconNonBuyableRussian;
 
     // Start is called before the first frame update
     void Start()
     {
         _uiScript = FindObjectOfType<GameUIScript>();
         _gameLogic = FindObjectOfType<GameLogic>();
-        _playerScript = FindObjectOfType<PlayerScript>();
-        _buttonImage = GetComponent<Image>();
-        ChangeSide();
-    }
-    
-    void FixedUpdate()
-    {
-        enabled = _playerScript.money >= _currentUnit.unitCost;
-        _buttonImage.sprite = _playerScript.money >= _currentUnit.unitCost ? 
-            _currentUnitIconBuyable : _currentUnitIconNonBuyable;
-        unitPriceText.color = _playerScript.money >= _currentUnit.unitCost ? Color.green : Color.red;
     }
 
     public void OnMouseEnter()
     {
-        _uiScript.bottomPanel.unitName.text = SettingsScript.SideIsSoviet ? unitUsa.name : unitRussian.name;
-        _uiScript.bottomPanel.attack.value = SettingsScript.SideIsSoviet ? unitUsa.attack : unitRussian.attack;
-        _uiScript.bottomPanel.fireRate.value = SettingsScript.SideIsSoviet ? unitUsa.fireRate : unitRussian.fireRate;
-        _uiScript.bottomPanel.hp.value = SettingsScript.SideIsSoviet ? unitUsa.maxHealth : unitRussian.maxHealth;
-        _uiScript.bottomPanel.speed.value = SettingsScript.SideIsSoviet ? unitUsa.speed : unitRussian.speed;
-        _uiScript.bottomPanel.attackValue.text = _uiScript.bottomPanel.attack.value.ToString();
-        _uiScript.bottomPanel.fireRateValue.text = _uiScript.bottomPanel.fireRate.value.ToString();
-        _uiScript.bottomPanel.hpValue.text = _uiScript.bottomPanel.hp.value.ToString();
-        _uiScript.bottomPanel.speedValue.text = _uiScript.bottomPanel.speed.value.ToString();
+        #warning Delete TryCatch when all units will be in game
+        try
+        {
+            _uiScript.unitName.text = _gameLogic.playingAsRussian ? unitUsa.name : unitRussian.name;
+            _uiScript.attack.value = _gameLogic.playingAsRussian ? unitUsa.attack : unitRussian.attack;
+            _uiScript.fireRate.value = _gameLogic.playingAsRussian ? unitUsa.fireRate : unitRussian.fireRate;
+            _uiScript.hp.value = _gameLogic.playingAsRussian ? unitUsa.maxHealth : unitRussian.maxHealth;
+            _uiScript.speed.value = _gameLogic.playingAsRussian ? unitUsa.speed : unitRussian.speed;
+            _uiScript.attackValue.text = _uiScript.attack.value.ToString();
+            _uiScript.fireRateValue.text = _uiScript.fireRate.value.ToString();
+            _uiScript.hpValue.text = _uiScript.hp.value.ToString();
+            _uiScript.speedValue.text = _uiScript.speed.value.ToString();
+        }
+        catch (NullReferenceException e)
+        {
+            if (transform.gameObject.name == "Unit3Image" || transform.gameObject.name == "Unit4Image")
+            {
+                Debug.LogWarning("We don't have this unit in game currently. This error is to be expected.");
+            }
+            else
+            {
+                Debug.LogError(e.Message);
+            }
+        }
     }
 
     public void OnMouseExit()
     {
-        _uiScript.bottomPanel.attack.value = 0;
-        _uiScript.bottomPanel.attackValue.text = "-----";
-        _uiScript.bottomPanel.fireRate.value = 0;
-        _uiScript.bottomPanel.fireRateValue.text = "-----";
-        _uiScript.bottomPanel.hp.value = 0;
-        _uiScript.bottomPanel.hpValue.text = "-----";
-        _uiScript.bottomPanel.speed.value = 0;
-        _uiScript.bottomPanel.speedValue.text = "-----";
-        _uiScript.bottomPanel.unitName.text = "N/A";
-    }
-
-    public void ChangeSide()
-    {
-        unitPriceText.text = (SettingsScript.SideIsSoviet ? unitRussian.unitCost : unitUsa.unitCost) 
-                             + SettingsScript.UIMoneyMark;
-        _currentUnit = SettingsScript.SideIsSoviet ? unitRussian : unitUsa;
-        _currentUnitIconBuyable = SettingsScript.SideIsSoviet ? 
-            UnitIconBuyableRussian : UnitIconBuyableUsa;
-        _currentUnitIconNonBuyable = SettingsScript.SideIsSoviet ? 
-            UnitIconNonBuyableRussian : UnitIconNonBuyableUsa;
+        _uiScript.attack.value = 0;
+        _uiScript.attackValue.text = "-----";
+        _uiScript.fireRate.value = 0;
+        _uiScript.fireRateValue.text = "-----";
+        _uiScript.hp.value = 0;
+        _uiScript.hpValue.text = "-----";
+        _uiScript.speed.value = 0;
+        _uiScript.speedValue.text = "-----";
+        _uiScript.unitName.text = "N/A";
     }
 }
