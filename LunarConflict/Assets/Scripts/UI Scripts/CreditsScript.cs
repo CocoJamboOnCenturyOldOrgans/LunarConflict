f77382525af155cdf9fceec6cd4a07e093b99afb
Skeletonek <skeleton0199@gmail.com>
2022-12-20 13:05:01 +0100

    Fields serialization in CreditsScript

--- LunarConflict/Assets/Scripts/UI Scripts/CreditsScript.cs
+++ LunarConflict/Assets/Scripts/UI Scripts/CreditsScript.cs
@@ -3,18 +3,14 @@ using UnityEngine.UI;
 
 public class CreditsScript : MonoBehaviour
 {
-    private Text _version;
-    private Text _unity; 
-    private Text _systemInformation;
+    [SerializeField] private Text version;
+    [SerializeField] private Text unity; 
+    [SerializeField] private Text systemInformation;
     void Start()
     {
-        _version = transform.Find("About_Version").GetComponent<Text>();
-        _unity = transform.Find("About_Unity").GetComponent<Text>();
-        _systemInformation = transform.Find("About_SystemInfo").GetComponent<Text>();
-        
-        _version.text = "Version: " + Application.version;
-        _unity.text = "Made in: Unity " + Application.unityVersion;
-        _systemInformation.text = "OS: " + SystemInfo.operatingSystem + "\n" +
+        version.text = "Version: " + Application.version;
+        unity.text = "Made in: Unity " + Application.unityVersion;
+        systemInformation.text = "OS: " + SystemInfo.operatingSystem + "\n" +
                                  "Graphics API: " + SystemInfo.graphicsDeviceType + " (" + SystemInfo.graphicsDeviceVersion + ")";
     }
 }
